dc2935f54f56ee288167e864b924055f79dc773b
Trevor <Trevor@TCPs-PC.home>
2023-11-30 17:35:06 -0500

    Added comment to test the commit feature

---
+++ src/HangmanApp.java
@@ -0,0 +1,27 @@
+/*
+Editor: Trevor Pence
+Date: 11/20/23
+Description: Modify the hangman app to include a hangman() method to 
+    display the hanged man
+*/
+
+// this is a comment to test the commit feature.
+import java.util.Scanner;
+
+public class HangmanApp {
+
+    public static void main(String[] args) {
+        System.out.println("Play the H A N G M A N game");
+        
+        Scanner sc = new Scanner(System.in);        
+        String again = "y";        
+        while(again.equalsIgnoreCase("y")) {
+            var hangman = new Hangman();
+            hangman.drawHangman(7);
+            hangman.playGame();
+            System.out.println();
+            System.out.print("Play again? (y/n): ");
+            again = sc.nextLine();
+        }
+    } 
+}
